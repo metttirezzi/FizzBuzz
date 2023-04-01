9ff8bd0342d128e7ddd5e891224003ecbb664bab
Mahdi <51413761+metttirezzi@users.noreply.github.com>
2023-04-01 16:44:26 +0330

    Add files via upload

---
+++ iteration.js
@@ -0,0 +1,13 @@
+// iteration in array 
+const numbers = [1 , 2 , 3];  // this is your  array 
+
+
+// using for loop 
+for ( let number of numbers )
+   console.log(number);
+// you can use varies loops in javascript for iteration 
+
+// using forEach method 
+numbers.forEach( number => {
+    console.log(number);
+});
