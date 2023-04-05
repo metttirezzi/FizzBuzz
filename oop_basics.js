03d0c4ebd150e2568208f7b75be9d70c1641f453
Mahdi <51413761+metttirezzi@users.noreply.github.com>
2023-04-05 11:22:26 +0330

    Add files via upload

---
+++ oop_basics.js
@@ -0,0 +1,13 @@
+const circle = {
+    radius : 1 ,
+    location : {  // these are properties 
+        // we are using object literal syntax
+        x : 1 ,
+        y : 1 ,
+    },
+    draw: function() {
+        console.log('draw');  // these are methods 
+    }
+}; // this is the object literal syntax
+
+circle.draw();
